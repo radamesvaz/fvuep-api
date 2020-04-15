d9b213dccd3a7a5763a83a49c501a9646396cd2d
Radames Vaz <rgvtovar@hotmail.com>
2020-04-15 19:20:11 -0400

    adding db

--- server/routs/api/post.js
+++ server/routs/api/post.js
@@ -4,10 +4,20 @@ const mongodb = require('mongodb');
 const router = express.Router();
 
 //Get Posts
-router.get('/', (req, res) => {
-    res.json('Im alive at post ;)')
+router.get('/', async (req, res) => {
+    const posts = await loadPostsCollection();
+    res.json(await posts.find({}).toArray());
 })
 
+
+async function loadPostsCollection(){
+    const client = await mongodb.MongoClient.connect
+    ('mongodb+srv://abc123:test@cluster0-4a0nc.mongodb.net/test?retryWrites=true&w=majority', {
+        useNewUrlParser: true
+    });
+
+    return client.db('vue_express').collection('post');
+}
 
 
 module.exports = router;
