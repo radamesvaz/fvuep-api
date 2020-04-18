314d578957ad78ef1aaf9efde61a2f591260c2a0
Radames Vaz <rgvtovar@hotmail.com>
2020-04-18 17:02:52 -0400

    preparing SPA

--- server/index.js
+++ server/index.js
@@ -12,6 +12,15 @@ const post = require('./routs/api/post');
 
 app.use('/api/post', post);
 
+//Handle Production
+if(process.env.NODE_ENV === 'production'){
+    //static folder
+    app.use(express.static(__dirname + '/public'));
+
+    //handle SPA
+    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`I'm alive at port ${port}`));
